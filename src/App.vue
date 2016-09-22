4178f0f3c4c5939ca98fa28d2bfede2f9e6fe61a
Mārcis <akonens@inbox.lv>
2016-09-22 21:47:53 +0300

    Izmainas

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,27 @@
 <template>
-
+  <input type="text" v-model="username"> <br>
+  <input type="text"> <br>
+  {{username.length}}
+  <div v-if="username.lenght">
+    Saulit debesis spiid
+  </div>
 </template>
 
 <script>
+export default {
+  data () {
+    return {
+      username: ""
+    }
+  }
+}
+submit () {
+  if(this.username == "suka") {
+    alert(fuck off)
+  }
+}
+
+
 
 </script>
 
