a2129ba7d40da63bb4f890c26b457fab002c54a1
Mārcis <akonens@inbox.lv>
2016-10-04 22:02:58 +0300

    js finish

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,47 @@
 <template>
-  <input type="text" v-model="username"> <br>
-  <input type="text"> <br>
-  {{username.length}}
-  <div v-if="username.lenght">
-    Saulit debesis spiid
-  </div>
+  <ul>
+
+    <li v-for="item in menu"
+        v-bind:class="{active: item.active}"
+        @click="goToPage(item)">
+      {{ item.title }}
+    </li>
+  </ul>
+
+
 </template>
 
 <script>
 export default {
   data () {
     return {
-      username: ""
+      menu: [
+        {
+          title: "Home",
+          active: true,
+        },
+        {
+          title: "Contats",
+          active: false,
+        },
+      ]
+    }
+  },
+  methods: {
+    goToPage (menuitem) {
+      for (let i in this.menu)
+      {
+        this.menu[i].active = false
+      }
+      menuitem.active = true
     }
   }
 }
+
 </script>
 
 <style>
-
+  .active {
+    color:red;
+  }
 </style>
