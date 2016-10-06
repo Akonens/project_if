371fadf62dc6299918b8fb76a25a84e5376aa40a
Mārcis <akonens@inbox.lv>
2016-10-06 20:39:09 +0300

    new project

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,11 @@
 <template>
-  <ul>
-
-    <li v-for="item in menu"
-        v-bind:class="{active: item.active}"
-        @click="goToPage(item)">
-      {{ item.title }}
-    </li>
-  </ul>
-
 
 </template>
 
 <script>
-export default {
-  data () {
-    return {
-      menu: [
-        {
-          title: "Home",
-          active: true,
-        },
-        {
-          title: "Contats",
-          active: false,
-        },
-      ]
-    }
-  },
-  methods: {
-    goToPage (menuitem) {
-      for (let i in this.menu)
-      {
-        this.menu[i].active = false
-      }
-      menuitem.active = true
-    }
-  }
-}
-
+export default {}
 </script>
 
 <style>
-  .active {
-    color:red;
-  }
+
 </style>
