8b688a71f4a1953b584d539d575ba87723133b96
Mārcis <akonens@inbox.lv>
2016-10-06 21:45:41 +0300

    new updates

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,42 @@
 <template>
+  <h1 class="name">Plate number</h1> {{ car.plateNumber }}
+  <h2 class="name2">Hourse power</h2> <p> {{ car.hoursepower }} </p>
 
+  <span v-if="iscoolCar(car)"> Ahujenna </span>
+  <span v-if="isbadCar(car)"> Leta masina </span>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      car: {
+        plateNumber: "AA-3333",
+        hoursepower: 1200,
+        price: 77,
+      }
+    }
+  },
+  methods: {
+    iscoolCar (car) {
+      console.log(car)
+      return (car.hoursepower > 1000)
+    },
+    isbadCar (car2) {
+      return (car2.price < 3000 )
+    }
+  }
+}
 </script>
 
 <style>
-
+  .name {
+    color:red;
+  }
+  .name2 {
+    color:blue;
+  }
+  p {
+    color:pink;
+  }
 </style>
