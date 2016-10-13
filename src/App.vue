6b83910b9aeb402fb1603ecb6a93466ffca2dde7
Mārcis <akonens@inbox.lv>
2016-10-13 21:14:29 +0300

    Mans....

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,15 @@
 <template>
-  <input v-model="user.name">
-  <input type="password" v-model="user.pass">
 
-  <button @click="authorize(user.name, user.pass)">Log In</button>
-
-  <pre>{{ user | json }}</pre>
-    {{ user | json }}
-  </pre>
 </template>
 
 <script>
-export default {
-  data () {
-    return {
-      user: {
-        name: "",
-        pass: ""
-      }
-    }
-  },
-  methods: {
-    authorize (username, password)  {
-      console.log (username)
-      if(username == "admin" && password == "admin123") {
-        alert ("You are admin")
-      }
-    }
-  }
+
+var counter=1;
+while(counter<=10) {
+    document.write("<p>Tis ir paragraph "+counter+"</p>");
+    counter++;
 }
+
 </script>
 
 <style>
