9eee7e983c4aeecd8dce4ede854ecdaa47156241
Mārcis <akonens@inbox.lv>
2016-10-20 19:20:17 +0300

    login

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,47 @@
 <template>
+  <div class="color" v-if="user.isAdmin">
+    Es esmu admins
+    <strong>{{user.name}}</strong>
+    <button @click="logout()">Logout</button>
+  </div>
+  <div v-if="!user.isAdmin">
+    Es esmu parasts lietotajs
 
+    <input v-model="user.name">
+    <input type="password" v-model="user.pass">
+
+    <button @click="authorize(user.name, user.pass)">Log In</button>
+  </div>
 </template>
-
 <script>
-
-var counter=1;
-while(counter<=10) {
-    document.write("<p>Tis ir paragraph "+counter+"</p>");
-    counter++;
+export default {
+  data () {
+    return {
+      user: {
+        name: "",
+        pass: "",
+        isAdmin: false,
+      },
+    }
+  },
+  methods: {
+    authorize (username, password)  {
+      if(username == "admin" && password == "z") {
+        this.user.isAdmin = true
+      } else {
+        this.user.pass = ""
+      }
+    },
+    logout () {
+      this.user.isAdmin = false
+    }
+  }
 }
-
 </script>
 
 <style>
+.color {
+
+}
+
 </style>
